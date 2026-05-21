54ad63bbf446c18884381f58794f7b15d837ec17
Bittu Gurrala <bittugurrala241@gmail.com>
2026-05-21 22:11:43 +0530

    Added homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
-
+import Link from "next/link"
 export default function Home() {
   return (
-    <div>This is the Homepage</div>
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <Link href= "/login">
+        <button className="bg-black p-4 rounded text-xl active:bg-gray-400">Click to Login</button>
+      </Link>
+    </div>
+    
   )  
 }
